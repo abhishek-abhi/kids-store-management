9391d978a6df37ddb6620a3db38f7c8bedebc3f7
Abhishek Kumar Mandal <Abhishek_Kumar_Mandal@epam.com>
2019-05-17 15:55:02 +0530

    Updated files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,35 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use View;
+use Auth;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        //
+        Schema::defaultStringLength(191);
+        View::share('Name',"Rick");
+        View::composer('*',function($view){
+            $view->with('userData',Auth::user());
+
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
